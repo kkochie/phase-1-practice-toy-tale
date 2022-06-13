c234ea536dd3b74486d530fde7f5f58a987130ff
Kristine Kochie <kristinekochie@gmail.com>
2022-06-12 20:15:44 -0400

    Fetch toys and create cards

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Fetch requests
+  function fetchResource(url) {
+    return fetch(url)
+    .then(res => res.json())
+  }
+
+  //Rendering functions
+  function createToy(toyData) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const btn = document.createElement('button');
+
+    h2.textContent = toyData.name;
+    img.src = toyData.image;
+    p.textContent = `${toyData.likes} likes`;
+    btn.textContent = "like";
+    btn.id = toyData.id;
+
+    btn.classList = 'like-btn';
+    div.classList = 'card';
+    img.classList = 'toy-avatar';
+
+    div.append(h2, img, p, btn);
+    document.querySelector('#toy-collection').append(div);
+  }
+
+  //Invoke functions
+  fetchResource('http://localhost:3000/toys')
+  .then(toys => toys.forEach(createToy))
+  
+
 });
+
